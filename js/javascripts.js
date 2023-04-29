829faa68b7ba8cdf42078b76695bfb0831eb0841
MatteoArMed <mate.araneda@duocuc.cl>
2023-04-29 18:10:37 -0400

    Agregando estilo a la caja de registros

--- js/javascripts.js
+++ js/javascripts.js
@@ -1,14 +1,24 @@
+// Variables pagina 'Contacto'
 var email = document.getElementById('correo');
 var direccion = document.getElementById('direccoin');
 var ciudad = document.getElementById('ciudad');
 var comuna = document.getElementById('comuna');
 var numero = document.getElementById('numero');
 var descripcion = document.getElementById('descripcion');
+
+// Variables pagina 'Registro'
+
+var nombres = document.getElementById('Nombres');
+var Apellidos = document.getElementById('Apellidos');
+var Contraseña = document.getElementById('Contraseña');
+var Correo = document.getElementById('Correo')
+var Terminos = document.getElementById('Terminos');
+
+// Variable color 'Error'
 var error = document.getElementById('Error');
 
-error.style.color = red;
-
-function EnviarFormulario(){
+// Funcion Datos 'Contacto'
+function EnviarFormularioContacto(){
     console.log('Enviando Formulario...');
 
     
@@ -26,6 +36,37 @@ function EnviarFormulario(){
     return false;
 }
 
+// Funcion datos 'Registro'
+function EnviarFormularioRegistro(){
+    console.log('Enviando Formulario...');
 
+    
+    var mensajeError = [];
+    
+    if(Nombres.value === null || Nombres.value === ''){
+        mensajeError.push('Debes ingresar un Nombre');
+    }
+    if(Apellidos.value === null || Apellidos.value === ''){
+        mensajeError.push('Debes ingresar un Apellido');
+    }
+    // error.innerHTML = mensajeError.join(', ');
+    
+    return false;
+}
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("formulario").addEventListener('submit', validarFormulario);
+});
 
-
+function validarFormulario() {
+    var Nombres = document.getElementById('Nombres').value;
+    if(Nombres.length == 0) {
+    alert('No has escrito nada en el Nombre');
+    return;
+    }
+    var Contraseña = document.getElementById('Contraseña').value;
+    if (Contraseña.length < 6) {
+    alert('La Contraseña no es válida');
+    return;
+    }
+    this.submit();
+}
